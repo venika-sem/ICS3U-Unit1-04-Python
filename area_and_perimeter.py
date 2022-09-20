804d73e92aa180561994b2c68a64edb68ee32d00
Venika Sem <venika.sem@mths.ca>
2022-09-20 14:46:25 +0000

    changed area_and_perimeter.py

---
+++ area_and_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by: Venika Sem
+# Created on: Sept 2020
+# This program calculates the area and perimeter of a rectangle
+#    with dimensions 5cm x 3cm
+
+
+def main():
+    # this function calculates the area and perimeter
+
+    print("If a rectangle has the dimensions: ")
+    print("5cm x 3cm")
+    print("")
+    print("The Area is {}cm².".format(3 * 5))
+    print("The Perimeter is {}cm.".format(2 * (3 + 5)))
+
+
+if __name__ == "__main__":
+    main()
